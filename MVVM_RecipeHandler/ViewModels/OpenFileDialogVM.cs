abd6574263531e3f5f4989a3aed703b7590d3816
Kurtlkebab <michibreitenberger@yahoo.de>
2022-04-28 13:14:37 +0200

    Refactoring OpenFileDialogVM

--- MVVM_RecipeHandler/ViewModels/OpenFileDialogVM.cs
+++ MVVM_RecipeHandler/ViewModels/OpenFileDialogVM.cs
@@ -13,12 +13,44 @@ using System.Threading.Tasks;
 
 namespace MVVM_RecipeHandler.ViewModels
 {
+    /// <summary>
+    /// Creates OpenFileDialog, builds string from chosen Image file
+    /// Derives from <see cref="ViewModelBase"/> class.
+    /// </summary>
     public class OpenFileDialogVM : ViewModelBase
     {
+        #region ------------- Fields, Constants, Delegates ------------------------
+        /// <summary>
+        /// Openfile Dialog Comman
+        /// </summary>
         public static RelayCommand OpenCommand { get; set; }
+
+        /// <summary>
+        /// Image converted to String
+        /// </summary>
         public string base64String;
+        
+        /// <summary>
+        /// the selected filepatch of openfiledialog
+        /// </summary>
         private string _selectedPath;
+
+        /// <summary>
+        /// the initial directory for OpenFileDialog we can specify
+        /// </summary>
+        private string _defaultPath;
+        #endregion-------------------------------------------------------------------
+
+        #region ------------- Constructor, Destructor, Dispose, Clone -------------
+
+        /// <summary>
+        /// Gets or sets the Base64string built from image
+        /// </summary>
         public string Base64String { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SelectedPatch from OpenfileDialog
+        /// </summary>
         public string SelectedPath
         {
             get { return _selectedPath; }
@@ -33,24 +65,41 @@ namespace MVVM_RecipeHandler.ViewModels
             }
         }
 
-        private string _defaultPath;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenFileDialogVM"/> class.
+        /// </summary>
+        /// /// <param name="eventAggregator">Event aggregator to communicate with other views via <see cref="Microsoft.Practices.Prism.Events"/> event types.</param>
         public OpenFileDialogVM(IEventAggregator eventAggregator) : base(eventAggregator)
         {
             RegisterCommands();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenFileDialogVM"/> class.
+        /// </summary>
+        /// <param name="defaultPath"> the initial directory for OpenfileDialgog</param>
+        /// <param name="eventAggregator">Event aggregator to communicate with other views via <see cref="Microsoft.Practices.Prism.Events"/> event types.</param>
         public OpenFileDialogVM(IEventAggregator eventAggregator,string defaultPath) : base(eventAggregator)
         {
             _defaultPath = defaultPath;
             RegisterCommands();
         }
 
+        #endregion-------------------------------------------------------------------
+
+        #region ------------- Private helper --------------------------------------
+        
+        /// <summary>
+        /// Instantiate new RelayCommand
+        /// </summary>
         private void RegisterCommands()
         {
             OpenCommand = new RelayCommand(ExecuteOpenFileDialog);
         }
 
+        /// <summary>
+        /// creates new OpenFileDialog and sets selectedPatch
+        /// </summary>
         private void ExecuteOpenFileDialog()
         {
             var dialog = new OpenFileDialog { InitialDirectory = _defaultPath };
@@ -58,6 +107,13 @@ namespace MVVM_RecipeHandler.ViewModels
 
             SelectedPath = dialog.FileName;
         }
+
+        /// <summary>
+        /// Converts an image to string
+        /// </summary>
+        /// <param name="image"> image to convert to string</param>
+        /// <param name="format"> defines the dataformat of the image</param>
+        /// <returns>Image as a string</returns>
         public string ImageToBase64String(Image image, ImageFormat format)
         {
             MemoryStream memory = new MemoryStream();
@@ -68,6 +124,11 @@ namespace MVVM_RecipeHandler.ViewModels
             return base64;
         }
 
+        /// <summary>
+        /// Converts a string to image
+        /// </summary>
+        /// <param name="base64"> string to convert to image/param>
+        /// <returns> Image built from base64 string</returns>
         public Image ImageFromBase64String(string base64)
         {
             MemoryStream memory = new MemoryStream(Convert.FromBase64String(base64));
@@ -76,5 +137,7 @@ namespace MVVM_RecipeHandler.ViewModels
 
             return result;
         }
+        #endregion-------------------------------------------------------------------
+
     }
 }
